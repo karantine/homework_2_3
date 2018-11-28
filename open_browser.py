1c74e04ae5462f901696d6d3581e6d4f8095e1dc
karantine <12798243+karantine@users.noreply.github.com>
2018-11-28 22:41:36 +0300

    Added open_browser.py

---
+++ open_browser.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+webbrowser.open('yandex.ru', new=2, autoraise=True)
+
+# Вариант с subprocess
+#
+# import subprocess
+#
+# process = subprocess.Popen("C:\\Users\\Username\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe")
